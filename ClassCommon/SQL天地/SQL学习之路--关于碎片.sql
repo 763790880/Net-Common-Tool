9e33c4aad80e6ea5c735922ff42437520fb5495e
cuichangfa <cuicf@sunits.com>
2019-06-03 17:55:40 +0800

    1

--- ClassCommon/SQL天地/SQL学习之路--关于碎片.sql
+++ ClassCommon/SQL天地/SQL学习之路--关于碎片.sql
@@ -22,6 +22,28 @@ declare @table_id int
 set @table_id=object_id('tb_Storage')
 --执行--查看当前对应表的碎片查询情况
 dbcc showcontig(@table_id)
+
+--DBCC SHOWCONTIG是显示指定的表的数据和索引的碎片信息。
+--解释如下：
+
+--Page Scanned-扫描页数：如果你知道行的近似尺寸和表或索引里的行数，那么你可以估计出索引里的页数。看看扫描页数，如果明显比你估计的页数要高，说明存在内部碎片。 
+
+--Extents Scanned-扫描扩展盘区数：用扫描页数除以8,四舍五入到下一个最高值。该值应该和DBCC SHOWCONTIG返回的扫描扩展盘区数一致。如果DBCC SHOWCONTIG返回的数高，说明存在外部碎片。碎片的严重程度依赖于刚才显示的值比估计值高多少。 
+
+--Extent Switches-扩展盘区开关数：该数应该等于扫描扩展盘区数减1。高了则说明有外部碎片。 
+
+--Avg. Pages per Extent-每个扩展盘区上的平均页数：该数是扫描页数除以扫描扩展盘区数，一般是8。小于8说明有外部碎片。 
+
+--Scan Density [Best Count:Actual Count]-扫描密度［最佳值:实际值］：DBCC SHOWCONTIG返回最有用的一个百分比。这是扩展盘区的最佳值和实际值的比率。该百分比应该尽可能靠近100％。低了则说明有外部碎片。
+
+--Logical Scan Fragmentation-逻辑扫描碎片：无序页的百分比。该百分比应该在0％到10％之间，高了则说明有外部碎片。 
+
+--Extent Scan Fragmentation-扩展盘区扫描碎片：无序扩展盘区在扫描索引叶级页中所占的百分比。该百分比应该是0％，高了则说明有外部碎片。 
+
+--Avg. Bytes Free per Page-每页上的平均可用字节数：所扫描的页上的平均可用字节数。越高说明有内部碎片，不过在你用这个数字决定是否有内部碎片之前，应该考虑fill factor（填充因子）。 
+
+--Avg. Page Density (full)-平均页密度（完整）：每页上的平均可用字节数的百分比的相反数。低的百分比说明有内部碎片
+
 --清理内部外部碎片
 DBCC INDEXDEFRAG(Sunits_Inventory_demo,tb_Storage)
 
